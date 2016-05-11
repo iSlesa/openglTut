aeab1a8710739ea05c3e2d544cfa0770585c6d5b
iSlesa <artycrab_slesh@yahoo.com>
2016-05-11 12:02:50 +0545

    add texture

--- main.cpp
+++ main.cpp
@@ -17,19 +17,27 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 const GLchar* vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 position;\n"
         "layout (location = 1) in vec3 ourcolor;\n"
+	"layout (location = 2) in vec2 textCoor;\n"
         "out vec3 vColor;\n"
+	"out vec2 texCoor;\n"
 	"void main()\n"
 	"{\n"
 	"gl_Position = vec4(position, 1.0);\n"
         "vColor = ourcolor;\n"
+	"texCoor = textCoor;\n"
 	"}\0";
 
 const GLchar* fragmentShaderSource= "#version 330 core\n"
 	"in vec3 vColor;\n"
+	"in vec2 texCoor;\n"
 	"out vec4 color;\n"
+
+	"uniform sampler2D ourTexture;\n"
+
 	"void main()\n"
 	"{\n"
-	"color = vec4(vColor, 1.0f);\n"
+//	"color = vec4(vColor, 1.0f);\n"
+	"color = texture(ourTexture, texCoor)* vec4(vColor, 1.0f);\n"
 	"}\0";
 
 int main(int argc, char** argv)
@@ -103,42 +111,63 @@ int main(int argc, char** argv)
     glDeleteShader(fragmentShader);
 
 //Setup the vertex data
-GLfloat vertices[] = {
-  0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // Left  
-    -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // Right 
-     0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f  // Top   
-};
+//GLfloat vertices[] = {
+//  0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // Left  
+//    -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // Right 
+//     0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f  // Top   
+//};
 //
-//    GLfloat vertices[] = {
-//	0.5f,  0.5f, 0.0f,  // Top Right
-//         0.5f, -0.5f, 0.0f,  // Bottom Right
-//        -0.5f, -0.5f, 0.0f,  // Bottom Left
-//        -0.5f,  0.5f, 0.0f   // Top Left 
-//    };
-//    GLuint indices[] = {  // Note that we start from 0!
-//        0, 1, 3,  // First Triangle
-//        1, 2, 3   // Second Triangle
-//    };
+    int w, h;
+    unsigned char* image = SOIL_load_image("container.jpg", &w, &h, 0, SOIL_LOAD_RGB);
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+        // Set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// Set texture wrapping to GL_REPEAT (usually basic wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // Set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    SOIL_free_image_data(image);
+    glBindTexture(GL_TEXTURE_2D, 0);
+	
+    GLfloat vertices[] = {
+         //position		//color		//texture
+	0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,  1.0f, 1.0f,  // Top Right
+         0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,// Bottom Right
+        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f,// Bottom Left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,  0.0f, 1.0f// Top Left 
+    };
+    GLuint indices[] = {  // Note that we start from 0!
+        0, 1, 3,  // First Triangle
+        1, 2, 3   // Second Triangle
+    };
     
-    GLuint VBO, VAO;
+    GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-  //  glGenBuffers(1, &EBO);
+    glGenBuffers(1, &EBO);
     //Bind
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);    
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-   // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-   // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
   //position 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
 //color
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3*sizeof(GLfloat)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3*sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
+    // textures
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8* sizeof(GLfloat), (GLvoid*)(6*sizeof(GLfloat)));
+    glEnableVertexAttribArray(2);
 
    // glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
@@ -157,11 +186,12 @@ GLfloat vertices[] = {
       
          // Draw our first triangle
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+     glBindTexture(GL_TEXTURE_2D, texture);   
+     glBindVertexArray(VAO);
+      //  glDrawArrays(GL_TRIANGLES, 0, 3);
        
-     //   glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-     glBindVertexArray(0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         // Swap the rendered screen to window.
         glfwSwapBuffers(window);
@@ -169,7 +199,7 @@ GLfloat vertices[] = {
      // Properly de-allocate all resources once they've outlived their purpose
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-  //  glDeleteBuffers(1, &EBO);
+    glDeleteBuffers(1, &EBO);
     
     // Once the main loop ends, exit by destroying the window.
     glfwTerminate();
